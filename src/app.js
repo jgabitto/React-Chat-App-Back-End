d54f135af03b86f3c51f85ca9c28cf099f3152de
Jorge Gabitto <jgabitto1792@gmail.com>
2020-11-10 12:09:02 -0600

    fixed origin

--- src/app.js
+++ src/app.js
@@ -36,7 +36,9 @@ app.use(express.urlencoded({ extended: true }));
 // res.set('Access-Control-Allow-Origin', req.headers.origin)
 // res.set('Access-Control-Allow-Credentials', 'true')
 // app.use(cors({ origin: true, credentials: true, exposedHeaders: 'Authorization' }));
-app.use(cors({ origin: 'https://react-chat-back-end.herokuapp.com/', credentials: true, exposedHeaders: 'Authorization' }));
+app.use(cors({
+  origin: 'https://jorge-chat-app.netlify.app/', credentials: true, exposedHeaders: 'Authorization'
+}));
 app.use(cookieParser());
 app.use(userRouter);
 app.use(chatRouter);
