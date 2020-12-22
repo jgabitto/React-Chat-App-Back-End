f0a0656936c10982e07ebb91bde02426fd4147f9
Jorge Gabitto <jgabitto1792@gmail.com>
2020-12-22 00:23:40 -0600

    added set-cookie

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 // res.set('Access-Control-Allow-Credentials', 'true')
 // app.use(cors({ origin: true, credentials: true, exposedHeaders: 'Authorization' }));
 app.use(cors({
-  origin: 'https://jorge-chat-app.netlify.app', credentials: true, exposedHeaders: 'Authorization', methods: ["GET,HEAD,PUT,PATCH,POST, OPTIONS"]
+  origin: 'https://jorge-chat-app.netlify.app', credentials: true, exposedHeaders: ['Authorization', 'Set-Cookie'], methods: ["GET,HEAD,PUT,PATCH,POST, OPTIONS"]
 }));
 app.set('trust proxy', 1)
 app.use(userRouter);
